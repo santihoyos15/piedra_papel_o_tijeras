e18562f58d284b4e32dca492eda4a028abb2da53
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-07-08 16:33:16 -0500

    Some comments

--- scissorsGame.js
+++ scissorsGame.js
@@ -3,15 +3,17 @@ function scissorsGame(usrAction){
 
     let randomNum = Math.floor(Math.random() * actions.length); //Chooses a random element of actions
     let pcAction = actions[randomNum]; 
+
     paragraph.innerHTML = `El computador elige ${pcAction}. <br>`
     console.log(usrAction);
+    
     if (usrAction === pcAction){
         return "Empate";
     }
     //User Wins 
-    else if(usrAction === actions[0] && pcAction === actions[2] ||
-            usrAction === actions[1] && pcAction === actions[0] ||
-            usrAction === actions[2] && pcAction === actions[1])
+    else if(usrAction === actions[0] && pcAction === actions[2] || //Chooses rock
+            usrAction === actions[1] && pcAction === actions[0] || //Chooses paper
+            usrAction === actions[2] && pcAction === actions[1])   //Chooses scissors
         {
         return "Ganaste!";
     }
