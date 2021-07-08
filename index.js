f6376968ed25d46fb41b3a056ae9af20aa664a3c
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-07-07 23:37:47 -0500

    scissorsGame function moved to a new file

--- index.js
+++ index.js
@@ -2,36 +2,3 @@ const actions = ["rock", "paper", "scissors"];
 
 let result = scissorsGame("rock");
 console.log(result);
-
-function scissorsGame(usrAction){
-    let randomNum = Math.floor(Math.random() * actions.length); //Chooses a random element of actions
-    let pcAction = actions[randomNum]; 
-    console.log(randomNum);
-    
-    //User chooses Rock
-    if (usrAction === actions[0] && pcAction === actions[0]){
-        return "Draw";
-    } else if (usrAction === actions[0] && pcAction === actions[1]){
-        return "PC Wins";
-    }else if (usrAction === actions[0] && pcAction === actions[2]){
-        return "User Wins";
-    }
-
-    //User chooses Paper
-    if (usrAction === actions[1] && pcAction === actions[0]){
-        return "User Wins";
-    } else if (usrAction === actions[1] && pcAction === actions[1]){
-        return "Draw";
-    }else if (usrAction === actions[1] && pcAction === actions[2]){
-        return "PC Wins";
-    }
-
-    //User chooses Scissors
-    if (usrAction === actions[2] && pcAction === actions[0]){
-        return "PC Wins";
-    } else if (usrAction === actions[2] && pcAction === actions[1]){
-        return "User Wins";
-    }else if (usrAction === actions[2] && pcAction === actions[2]){
-        return "Draw";
-    }
-}
