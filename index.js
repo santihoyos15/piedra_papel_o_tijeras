3da9fea2b6e9d44711a01995a6944bd98ccb5ace
santiagohoyos2808@gmail.com <santiagohoyos2808@gmail.com>
2021-07-10 20:49:17 -0500

    Bug on scissors's option corrected

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ paper.addEventListener("click", () => {printResult("papel")});
 
 //Scissors button
 let scissors = document.getElementById("scissors");
-scissors.addEventListener("click", () => {printResult("tijera")});
+scissors.addEventListener("click", () => {printResult("tijeras")});
 
 function printResult(action){
     result = scissorsGame(action); //receives one of these parameters: "rock", "paper", "scissors"
